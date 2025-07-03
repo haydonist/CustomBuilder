747df7415c4b8acd6f83c7f4c432319f922ce605
Chance Snow <git@chancesnow.me>
2025-07-03 18:13:31 -0500

    Extract app header and footer from wizard component, ...
    Also add [Material Symbols](https://fonts.google.com/icons) icon font

--- src/belt-wizard.ts
+++ src/belt-wizard.ts
@@ -1,11 +1,12 @@
 import { css, html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
-const logo = new URL("../../assets/open-wc-logo.svg", import.meta.url).href;
+// See // See https://open-wc.org
+// See https://open-wc.org/guides/developing-components/code-examples
 
 export enum Theme {
   light = "light",
-  dark = "dark"
+  dark = "dark",
 }
 
 @customElement("belt-wizard")
@@ -14,63 +15,23 @@ export class CustomBeltWizard extends LitElement {
   header = "Customize a Belt";
 
   static override styles = css`
-    :host {
-      font-size: calc(10px + 2vmin);
-      color: var(--neutral);
-      text-align: center;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      min-height: 100vh;
-      max-width: 960px;
-      margin: 0 auto;
-      background-color: var(--belt-wizard-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
     .logo {
       margin-top: 36px;
     }
 
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
+    :host {
+      text-align: center;
     }
   `;
 
+  // Disable the shadow DOM for this root-level component
+  override createRenderRoot() {
+    return this;
+  }
+
   override render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src="${logo}" /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/CustomBeltWizard.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        Made with ♥ by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-        >Jollyweb Consulting</a>.
-      </p>
+      <h1>${this.header}</h1>
     `;
   }
 }
