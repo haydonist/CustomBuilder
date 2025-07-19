9c66fd5d8d6e8482e365a4f60ba47c2afdf47f89
Chance Snow <git@chancesnow.me>
2025-07-19 02:03:26 -0500

    Add step properties to Wizard model

--- src/models/wizard/index.ts
+++ src/models/wizard/index.ts
@@ -10,8 +10,30 @@ export default class Wizard {
     return this.#step;
   }
 
+  get hasNextStep() {
+    return this.#step < this.steps.length - 1;
+  }
+
+  get hasPreviousStep() {
+    return this.#step > 0;
+  }
+
   get length() {
     return this.steps.length;
+  }
+
+  get currentStep() {
+    return this.steps[this.#step];
+  }
+
+  get previousStep() {
+    if (!this.hasPreviousStep) throw new Error("Cannot access step before the first!");
+    return this.steps[this.#step - 1];
+  }
+
+  get nextStep() {
+    if (!this.hasNextStep) throw new Error("Cannot access step after the last!");
+    return this.steps[this.#step + 1];
   }
 
   get currentView() {
@@ -26,6 +48,12 @@ export default class Wizard {
   previous() {
     assert(this.#step > 0, "Cannot go back past the first step!");
     this.#step -= 1;
+  }
+
+  goTo(i: number) {
+    assert(i <= this.steps.length - 1, "Cannot advance past the last step!");
+    assert(i >= 0, "Cannot go back past the first step!");
+    this.#step = i;
   }
 }
 
