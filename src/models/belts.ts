0446ec8f4707fa41ee8ee83bcf8d18d36f6b707c
haydonist <hayden@jollywebconsulting.com>
2025-10-17 15:27:28 -0600

    Add names to test data

--- src/models/belts.ts
+++ src/models/belts.ts
@@ -21,9 +21,9 @@ export const beltBases: ThumbnailOption[] = [
   { id: "4218968x14", name: "Coarse", thumbnail: "/assets/belts/black-speckled.png" },
   { id: "4218968x15", name: "Natural", thumbnail: "/assets/belts/tan-leather.png" },
   { id: "4218968x16", name: "Woven", thumbnail: "/assets/belts/black-speckled.png" },
-  { id: "4218968x17", thumbnail: "/assets/belts/tan-leather.png" },
-  { id: "4218968x18", thumbnail: "/assets/belts/tan-leather.png" },
-  { id: "4218968x19", thumbnail: "/assets/belts/tan-leather.png" },
+  { id: "4218968x17", name: "Natural 2", thumbnail: "/assets/belts/tan-leather.png" },
+  { id: "4218968x18", name: "Natural 3", thumbnail: "/assets/belts/tan-leather.png" },
+  { id: "4218968x19", name: "Natural 4", thumbnail: "/assets/belts/tan-leather.png" },
 ];
 
 export const beltColors: ColorOption[] = [
@@ -36,19 +36,19 @@ export const beltColors: ColorOption[] = [
 
 export const beltBuckles: ThumbnailOption[] = [
   { id: "2-in-brass", name: "2\" Brass", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
-  { id: "a", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
-  { id: "b", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
-  { id: "c", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
-  { id: "d", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
-  { id: "e", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
+  { id: "a", name: "2\" Brass", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
+  { id: "b", name: "2\" Brass", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
+  { id: "c", name: "2\" Brass", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
+  { id: "d", name: "2\" Brass", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
+  { id: "e", name: "2\" Brass", thumbnail: "/assets/belts/2-in-brass-buckle.png" },
 ];
 
 export const beltLoops: ThumbnailOption[] = [
   { id: "slate", name: "Slate 1/4\" Loop", thumbnail: "/assets/belts/belt-loop.png" },
-  { id: "a", thumbnail: "/assets/belts/belt-loop.png" },
-  { id: "b", thumbnail: "/assets/belts/belt-loop.png" },
-  { id: "c", thumbnail: "/assets/belts/belt-loop.png" },
-  { id: "d", thumbnail: "/assets/belts/belt-loop.png" },
+  { id: "a", name: "Slate 1/4\" Loop", thumbnail: "/assets/belts/belt-loop.png" },
+  { id: "b", name: "Slate 1/4\" Loop", thumbnail: "/assets/belts/belt-loop.png" },
+  { id: "c", name: "Slate 1/4\" Loop", thumbnail: "/assets/belts/belt-loop.png" },
+  { id: "d", name: "Slate 1/4\" Loop", thumbnail: "/assets/belts/belt-loop.png" },
 ];
 
 export const beltConchos: ThumbnailOption[] = [
@@ -56,16 +56,16 @@ export const beltConchos: ThumbnailOption[] = [
   { id: "brass-flower", name: "Brass Flower", thumbnail: "assets/belts/conchos/brass-flower.png" },
   { id: "silver-brooch", name: "Silver Brooch", thumbnail: "assets/belts/conchos/silver-brooch.png" },
   { id: "studded-silver", name: "Studded Silver", thumbnail: "assets/belts/conchos/studded-silver.png" },
-  { id: "a", thumbnail: "assets/belts/conchos/brass-axe.png" },
-  { id: "b", thumbnail: "assets/belts/conchos/brass-flower.png" },
-  { id: "c", thumbnail: "assets/belts/conchos/silver-brooch.png" },
-  { id: "d", thumbnail: "assets/belts/conchos/studded-silver.png" },
+  { id: "a", name: "Brass Axe", thumbnail: "assets/belts/conchos/brass-axe.png" },
+  { id: "b", name: "Brass Flower", thumbnail: "assets/belts/conchos/brass-flower.png" },
+  { id: "c", name: "Silver Brooch", thumbnail: "assets/belts/conchos/silver-brooch.png" },
+  { id: "d", name: "Studded Silver", thumbnail: "assets/belts/conchos/studded-silver.png" },
 ];
 
 export const beltTips: ThumbnailOption[] = [
   { id: "silver-tip", name: "Silver Tip", thumbnail: "assets/belts/silver-tip.png" },
-  { id: "a", thumbnail: "assets/belts/silver-tip.png" },
-  { id: "b", thumbnail: "assets/belts/silver-tip.png" },
-  { id: "c", thumbnail: "assets/belts/silver-tip.png" },
-  { id: "d", thumbnail: "assets/belts/silver-tip.png" },
+  { id: "a", name: "Silver Tip", thumbnail: "assets/belts/silver-tip.png" },
+  { id: "b", name: "Silver Tip", thumbnail: "assets/belts/silver-tip.png" },
+  { id: "c", name: "Silver Tip", thumbnail: "assets/belts/silver-tip.png" },
+  { id: "d", name: "Silver Tip", thumbnail: "assets/belts/silver-tip.png" },
 ];
